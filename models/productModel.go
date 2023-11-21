389c29153c4494dda760c868ce6a9a9da56bbc24
MatiasGalli <matias.galli@alumnos.ucn.cl>
2023-11-21 17:22:35 -0300

    .

---
+++ models/productModel.go
@@ -0,0 +1,20 @@
+package models
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type Product struct {
+	gorm.Model
+
+	ID          uuid.UUID `json:"id" gorm:"primary_key"`
+	CategoryID  string    `json:"category_id"`
+	Name        string    `json:"name"`
+	Price       float32   `json:"price"`
+	Stock       int       `json:"stock"`
+	Description string    `json:"description"`
+	Offer       bool      `json:"offer"`
+	Promotion   float32   `json:"promotion"`
+	Image       string    `json:"image"`
+}
