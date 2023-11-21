fd3bb68470d033857f1b53f0d487f98ca009a5eb
MatiasGalli <matias.galli@alumnos.ucn.cl>
2023-11-21 20:52:55 -0300

    Add product CRUD operations and message handling

--- controllers/productController.go
+++ controllers/productController.go
@@ -1,1 +1,34 @@
 package controllers
+
+import (
+	db "github.com/MatiasGalli/MS_Product/config"
+	"github.com/MatiasGalli/MS_Product/models"
+)
+
+// CreateProduct creates a product
+func CreateProduct(product models.Product) (models.Product, error) {
+	result := db.DB.Create(&product)
+	return product, result.Error
+}
+func GetProducts() ([]models.Product, error) {
+	var products []models.Product
+	result := db.DB.Find(&products)
+	return products, result.Error
+}
+
+func GetProduct(productID string) (models.Product, error) {
+	var product models.Product
+	result := db.DB.Where(&product, "id = ?", productID)
+	return product, result.Error
+}
+
+func UpdateProduct(productID, product models.Product) (models.Product, error) {
+	result := db.DB.Save(&product).Where("id = ?", productID)
+	return product, result.Error
+}
+
+func DeleteProduct(productID string) error {
+	var product models.Product
+	result := db.DB.Delete(&product, "id = ?", productID)
+	return result.Error
+}
--- internal/handler.go
+++ internal/handler.go
@@ -1,1 +1,104 @@
 package internal
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"time"
+
+	"github.com/MatiasGalli/MS_Product/controllers"
+	"github.com/MatiasGalli/MS_Product/models"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+// Handler is a struct that contains the methods to handle the messages
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+func Handler(d amqp.Delivery, channel *amqp.Channel) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	var response models.Response
+
+	actionType := d.Type
+	switch actionType {
+	case "create_product":
+		log.Println("Creating product")
+
+		var product models.Product
+		err := json.Unmarshal(d.Body, &product)
+		failOnError(err, "Failed to unmarshal product")
+		productJSON, err := json.Marshal(product)
+		failOnError(err, "Failed to marshal product")
+
+		_, err = controllers.CreateProduct(product)
+		if err != nil {
+			response = models.Response{
+				Success: "error",
+				Message: "Failed to create product",
+				Data:    []byte(err.Error()),
+			}
+		} else {
+			response = models.Response{
+				Success: "success",
+				Message: "Product created successfully",
+				Data:    productJSON,
+			}
+		}
+	case "get_products":
+		log.Println("Getting products")
+
+		product, err := controllers.GetProducts()
+		failOnError(err, "Failed to get products")
+		productsJSON, err := json.Marshal(product)
+		failOnError(err, "Failed to marshal products")
+
+		response = models.Response{
+			Success: "success",
+			Message: "Products retrieved successfully",
+			Data:    productsJSON,
+		}
+
+	case "get_product":
+		log.Println("Getting product")
+		var data struct {
+			ID string `json:"id"`
+		}
+
+		err := json.Unmarshal(d.Body, &data)
+		failOnError(err, "Failed to unmarshal product")
+
+		product, err := controllers.GetProduct(data.ID)
+		failOnError(err, "Failed to get product")
+		productJSON, err := json.Marshal(product)
+		failOnError(err, "Failed to marshal product")
+
+		response = models.Response{
+			Success: "success",
+			Message: "Product retrieved successfully",
+			Data:    productJSON,
+		}
+	}
+
+	responseJSON, err := json.Marshal(response)
+	failOnError(err, "Failed to marshal response")
+
+	err = channel.PublishWithContext(ctx,
+		"",
+		d.ReplyTo,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType:   "application/json",
+			CorrelationId: d.CorrelationId,
+			Body:          responseJSON,
+		})
+	failOnError(err, "Failed to publish a message")
+
+	d.Ack(false)
+}
